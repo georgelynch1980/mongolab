3b04c956509a91c40916e8692ccb42621bde27a1
georgelynch <georgelynch1980@hotmail.com>
2015-06-01 16:49:36 +0800

    bbbb

--- api/controllers/ProjectsController.js
+++ api/controllers/ProjectsController.js
@@ -9,14 +9,13 @@ module.exports = {
 
 
     add: function (req, res, next) {
-        /*
+
         return res.view('add', {
             layout: 'layout'
         });
-        */
+
     },
     create: function (req, res, next) {
-        /*
         var params = req.params.all();
 
         Projects.create(params, function (err, data) {
@@ -25,16 +24,15 @@ module.exports = {
                 layout: 'layout'
             });
 
-        });*/
+        });
 
     },
     find: function (req, res, next) {
-        /*
+
         Projects.find(function (err, data) {
             if (data === undefined) return res.notFound();
             if (err) return next(err);
             res.json(data);
         });
-        */
     }
 };
